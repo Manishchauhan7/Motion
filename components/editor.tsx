8486d3c2418087b4ad3fb459f8e3e3e77117127e
David Dubbldam <manishmrj7081@gmail.com>
2024-07-20 17:09:15 +0530

    fixed issue save on reload

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Block, BlockNoteEditor, PartialBlock } from "@blocknote/core";
+import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
 import "@blocknote/core/fonts/inter.css";
 import { BlockNoteView } from "@blocknote/mantine";
 import "@blocknote/core/style.css";
@@ -8,47 +8,39 @@ import { useCreateBlockNote } from "@blocknote/react";
 import { useTheme } from "next-themes";
 import { useEdgeStore } from "@/lib/edgestore";
 
-interface EditorProps{
-  onChange:(value:string)=>void;
-  initialContent?:string;
-  editable?:boolean;
-};
- const Editor=({
-  onChange,
-  initialContent,
-  editable
-}:EditorProps)=>{
+interface EditorProps {
+  onChange: (value: string) => void;
+  initialContent?: string;
+  editable?: boolean;
+}
+const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
+  const { resolvedTheme } = useTheme();
+  const { edgestore } = useEdgeStore();
 
-  const {resolvedTheme}=useTheme();
-  const {edgestore}=useEdgeStore();
-const handleUpload=async (file:File)=>{
-  const response= await edgestore.publicFiles.upload({
-    file
+  const handleUpload = async (file: File) => {
+    const response = await edgestore.publicFiles.upload({
+      file,
+    });
+    return response.url;
+  };
+
+  const editor: BlockNoteEditor = useCreateBlockNote({
+    initialContent: initialContent
+      ? (JSON.parse(initialContent) as PartialBlock[])
+      : undefined,
+
+    uploadFile: handleUpload,
   });
-  return response.url;
-}
 
-  const editor:BlockNoteEditor=useCreateBlockNote({
-    
-    initialContent: initialContent?(JSON.parse(initialContent) as PartialBlock[]):
-    undefined,
-  
-  uploadFile:handleUpload  
-  })
-
-
-  return(
+  return (
     <div>
-
-    
-    <BlockNoteView
-    editor={editor}
-    editable={editable}
-    theme={resolvedTheme==="dark" ? "dark": "light"}
-    />
+      <BlockNoteView
+        editor={editor}
+        onChange={() => {
+          onChange(JSON.stringify(editor.document, null, 2));
+        }}
+      />
     </div>
-      
-    
-  )
-}
+  );
+};
 export default Editor;
